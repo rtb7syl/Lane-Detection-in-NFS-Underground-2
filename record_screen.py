b654cbfb5080838db3fff97737c236ddeb62be62
sairajk <sairajinorganic@gmail.com>
2018-07-31 02:12:35 +0530

    record screen and detect edges, detection still at initial stage

---
+++ record_screen.py
@@ -0,0 +1,26 @@
+import cv2
+import numpy as np
+from PIL import ImageGrab
+import time
+
+
+def process_img(image):
+    original_image = image
+    # convert to gray
+    processed_img = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    # edge detection
+    processed_img =  cv2.Canny(processed_img, threshold1 = 200, threshold2=300)
+    return processed_img
+
+
+last_time = time.time()
+while(True):
+    screen = np.array(ImageGrab.grab(bbox=(0, 40, 640, 510)))
+    print("Loop time : {}".format(time.time() - last_time))
+    last_time = time.time()
+    new_screen = process_img(screen)
+    cv2.imshow('Window', new_screen)
+    # cv2.imshow('Window', cv2.cvtColor(new_screen, cv2.COLOR_RGB2BGR))
+    if cv2.waitKey(25) & 0xFF == ord('q'):
+        cv2.destroyAllWindows()
+        break
