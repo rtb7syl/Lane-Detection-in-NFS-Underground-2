b0873b4b9d1828706ed7adb5558cec66e401fb03
sairajk <sairajinorganic@gmail.com>
2018-08-10 03:59:14 +0530

    Working on separating left and right lanes, incomplete

--- record_screen.py
+++ record_screen.py
@@ -3,6 +3,7 @@ import numpy as np
 from PIL import ImageGrab
 import time
 import math
+from scipy.interpolate import UnivariateSpline
 from direct_key_inputs import PressKey, ReleaseKey, W, A, S, D
 
 tl_width = 0  # top left width, i.e top left x coordinate
@@ -61,10 +62,70 @@ def draw_lines(img,lines):
                     right_line_x.extend([x1, x2])
                     right_line_y.extend([y1, y2])
 
-        for line in lines:
-            coords = line[0]
-            cv2.line(img, (coords[0], coords[1]), (coords[2], coords[3]),
-                     [255,255,255], 3)
+
+        print(left_line_x)
+        print(left_line_y)
+        print(right_line_x)
+        print(right_line_y)
+
+        left_temp_x = left_line_x.copy()
+        right_temp_x = right_line_x.copy()
+        
+        # Sort X values and accordingly y values
+        left_line_y = [y for _, y in sorted(zip(left_temp_x, left_line_y))]
+        left_line_x = sorted(left_line_x)
+
+        right_line_y = [y for _, y in sorted(zip(right_temp_x, right_line_y))]
+        right_line_x = sorted(right_line_x)
+
+        # Remove duplicate elements from X and acoordindly from Y
+        _, l_unq_idx = np.unique(left_line_x, return_index=True)
+        left_line_x = list(set(left_line_x))
+        left_line_y = [left_line_y[i] for i in l_unq_idx]
+
+        _, r_unq_idx = np.unique(right_line_x, return_index=True)
+        right_line_x = list(set(right_line_x))
+        right_line_y = [right_line_y[i] for i in r_unq_idx]
+
+        print(left_line_x)
+        print(left_line_y)
+        print(right_line_x)
+        print(right_line_y)
+
+        # Get Left lane coordinates
+        left_interpolator = UnivariateSpline(left_line_x, left_line_y, k=2)
+        ll_x_vals = np.linspace(left_line_x[0], left_line_x[-1], 5)
+        ll_y_vals = left_interpolator(ll_x_vals)
+        ll_coords = np.array([[round(x), round(y)] for x, y in zip(ll_x_vals, ll_y_vals)])
+        ll_coords = ll_coords.reshape((-1, 1, 2))
+        
+        # Get Right lane coordinates
+        right_interpolator = UnivariateSpline(right_line_x, right_line_y, k=2)
+        rl_x_vals = np.linspace(right_line_x[0], right_line_x[-1], 5)
+        rl_y_vals = right_interpolator(rl_x_vals)
+        rl_coords = np.array([[round(x), round(y)] for x, y in zip(rl_x_vals, rl_y_vals)])
+        rl_coords = rl_coords.reshape((-1, 1, 2))
+
+        cv2.polylines(img,[ll_coords], True, (0,255,255))
+        cv2.polylines(img,[rl_coords], True, (0,255,255))
+##        for line in lines:
+##            x1, y1, x2, y2 = coords[0], coords[1], coords[2], coords[3]
+##
+##            slope = (y2 - y1) / (x2 - x1 + 0.001)  # Calculating the slope.
+##            if math.fabs(slope) < 0.5:  # Only consider extreme slope
+##                continue
+##            if slope <= 0:  # If the slope is negative, left group.
+##                left_lane.append(coords)
+##            else:  # Otherwise, right group.
+##                right_lane.append(coords)
+##            
+##            for lane in left_lane:
+####                print(left_lane)
+##                lane_coords = lane[0]
+####                print(lane_coords)
+##                cv2.line(img, (lane[0], lane[1]),
+##                         (lane[2], lane[3]),
+##                         [255,255,255], 3)
 
 def main():
     # This gives us time to set things up
